40e7fd05552edce0e1ec329ee749fe24c94bacbc
Dokyun Claire Lim <im.koremp@gmail.com>
2025-02-28 16:25:15 +0900

    chore: update prettierrc to prettierrc.ts

---
+++ .prettierrc.ts
@@ -0,0 +1,13 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  "singleQuote": true,
+  "parser": "typescript",
+  "semi": true,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "useTabs": true,
+  "printWidth": 80
+};
+
+export default config;
