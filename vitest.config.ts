8e10a141506efacbc68fd4a0cbdb040a1300165d
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-03 14:34:48 +0900

    chore: add playwright browser vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+	plugins: [react()],
 	test: {
-		// ...
+		browser: {
+			enabled: true,
+			provider: 'playwright',
+			// https://vitest.dev/guide/browser/playwright
+			instances: [
+				{
+					browser: 'chromium',
+				},
+			],
+		},
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './vitest.setup.ts',
 	},
 });
